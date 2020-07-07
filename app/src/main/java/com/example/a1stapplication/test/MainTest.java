681f944e241db7491da82a9fdcd9d75b557a84a9
Tanmay <tanmay121071009@gmail.com>
2020-07-07 15:07:50 +0600

    office 07-jul

--- app/src/main/java/com/example/a1stapplication/test/MainTest.java
+++ app/src/main/java/com/example/a1stapplication/test/MainTest.java
@@ -24,29 +24,42 @@ public class MainTest extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test);
+        setContentView(R.layout.new_module_1);
         context = MainTest.this;
 
-        final TextInputEditText editTextLCItem8 = findViewById(R.id.editTextLCItem8);
+        final TextInputEditText checkboxLC_Ques38 = findViewById(R.id.checkboxLC_Ques38);
+        final TextInputEditText qus_40 = findViewById(R.id.checkboxLC_Ques40);
+        final TextInputEditText qus_49=findViewById(R.id.checkboxLC_Ques49);
+        final TextInputEditText qus_52=findViewById(R.id.checkboxLC_Ques52);
 
         // Date Picker
+datePicker(checkboxLC_Ques38);
+datePicker(qus_40);
+datePicker(qus_49);
+datePicker(qus_52);
+
+
+
+
+
+    }
+
+    public void datePicker(final TextInputEditText textInputEditText) {
         final DatePickerDialog datePickerDialog1 = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 String ui = dayOfMonth +"-"+(month+1) + "-" + year;
                 Log.e("Date", ui);
-                editTextLCItem8.setText(ui);
+                textInputEditText.setText(ui);
             }
         }, Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
-        editTextLCItem8.setOnClickListener(new View.OnClickListener() {
+        textInputEditText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!datePickerDialog1.isShowing())
                     datePickerDialog1.show();
             }
         });
-
-
     }
 
     public void tanvir(View viewp) {
