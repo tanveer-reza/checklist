7b2a97ee0c42c3b8ac48f46ac899f464d3a29868
Tanmay <tanmay121071009@gmail.com>
2020-08-06 16:46:42 +0600

    office 06-Aug

--- app/src/main/java/com/example/a1stapplication/test/MainTest.java
+++ app/src/main/java/com/example/a1stapplication/test/MainTest.java
@@ -8,7 +8,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.a1stapplication.MainActivity;
@@ -16,6 +18,8 @@ import com.example.a1stapplication.R;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.Calendar;
+
+import static com.example.a1stapplication.R.id.spinnerLCItem8;
 
 public class MainTest extends AppCompatActivity {
 
@@ -26,6 +30,83 @@ public class MainTest extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_module_1);
         context = MainTest.this;
+
+
+        Spinner spinner = findViewById(R.id.spinnerLCItem97_gha);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //String item_position = String.valueOf(position);
+                //int positonInt = Integer.valueOf(item_position);
+                if (position == 1) {
+                    findViewById(R.id.textViewLC_Title_qus_97_uma).setVisibility(View.VISIBLE);
+                    findViewById(R.id.checkboxLC_Ques97_uma).setVisibility(view.VISIBLE);
+                } else {
+                    findViewById(R.id.textViewLC_Title_qus_97_uma).setVisibility(View.GONE);
+                    findViewById(R.id.checkboxLC_Ques97_uma).setVisibility(view.GONE);
+                }
+                Toast.makeText(MainTest.this, "value is "+ position, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+
+        Spinner spinner1 = findViewById(R.id.spinnerLCItem7);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position==2){
+                    findViewById(R.id.textViewLC_Title2).setVisibility(view.VISIBLE);
+                    findViewById(R.id.spinnerLCItem8).setVisibility(view.VISIBLE);
+                } else {
+                    findViewById(R.id.textViewLC_Title2).setVisibility(view.GONE);
+                    findViewById(R.id.spinnerLCItem8).setVisibility(view.GONE);
+                    findViewById(R.id.textViewLCItem7_0_1).setVisibility(view.GONE);
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
+        Spinner spinner4 = findViewById(R.id.spinnerLCItem8);
+        spinner4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            private Object AdapterView;
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position==4){
+                    findViewById(R.id.textViewLCItem7_0_1).setVisibility(view.VISIBLE);
+
+                }  else {
+                    findViewById(R.id.textViewLCItem7_0_1).setVisibility(view.GONE);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        /*findViewById(R.id.spinnerLCItem97_gha).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                findViewById(R.id.linearLayoutLCItem7_0_1tem7_0_97_gha).setVisibility(View.VISIBLE);
+            }
+        });*/
 
         final TextInputEditText checkboxLC_Ques38 = findViewById(R.id.checkboxLC_Ques38);
         final TextInputEditText qus_40 = findViewById(R.id.checkboxLC_Ques40);
